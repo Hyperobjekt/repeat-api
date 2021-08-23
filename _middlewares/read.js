143ba86b2e4bf71d9beb8b4c9dbcbf58a6fc7eb1
Uri Maxima <uri@maxima.ninja>
2021-08-23 08:19:16 -0400

    update

--- _middlewares/read.js
+++ _middlewares/read.js
@@ -1,14 +1,23 @@
 module.exports = (req, res, next) => {
-  const read = require('../_lib/ccrud/read');
-  let db = req.app.get('db');
-  let collection = req.baseUrl.split('/')[2];
-
-  read(db, req.schemas[collection], collection, req.locals.query, req.locals.skip || 0, req.locals.limit || 200, req.locals.sort, req.body.expand)
-    .then(response => {
+  const read = require("../_lib/ccrud/read");
+  let db = req.app.get("db");
+  let collection = req.baseUrl.split("/")[2];
+  read(
+    db,
+    req.schemas[collection],
+    collection,
+    req.locals.query,
+    req.locals.skip || 0,
+    req.locals.limit || 200,
+    req.locals.sort,
+    req.body.expand
+  )
+    .then((response) => {
       let paramKeys = Object.keys(req.params);
-      paramKeys.length === 1 && paramKeys[0] === '_id'
-        ? res.locals[collection] = response[0]
-        : res.locals[collection] = response;
-      next()
-    }).catch(err => console.log(err))
-}
+      paramKeys.length === 1 && paramKeys[0] === "_id"
+        ? (res.locals[collection] = response[0])
+        : (res.locals[collection] = response);
+      next();
+    })
+    .catch((err) => console.log(err));
+};
--- services/scenarios/scenarios.js
+++ services/scenarios/scenarios.js
@@ -1,13 +1,40 @@
-const router = require('express').Router();
-const _m = require('../../_middlewares');
+const router = require("express").Router();
+const _m = require("../../_middlewares");
 
-router.get('/', _m.setQuery, _m.setPagination, _m.read, _m.count, _m.done);
-router.post('/', _m.setQuery, _m.setPagination, _m.read, _m.count, _m.done);
-router.get('/csv', _m.setQuery, _m.setPagination, _m.read, _m.count, _m.doneCsv);
-router.get('/count', _m.setQuery, _m.count, _m.done);
-router.get('/:_id', _m.setQuery, _m.read, _m.done);
-router.post('/create', _m.protect, _m.create, _m.done);
-router.put('/:_id', _m.protect, _m.setQuery, _m.update, _m.done);
-router.delete('/:_id', _m.protect, _m.setQuery, _m.update, _m.delete, _m.create, _m.done);
+const data = require('../../_data/repeat_data.json')
+router.get('/', (req, res) => {
+  res.status(200).json(data)
+});
+
+// router.get(
+//   "/",
+//   _m.setQuery,
+//   _m.setPagination,
+//   _m.read,
+//   _m.count,
+//   _m.done
+// );
+router.post("/", _m.setQuery, _m.setPagination, _m.read, _m.count, _m.done);
+router.get(
+  "/csv",
+  _m.setQuery,
+  _m.setPagination,
+  _m.read,
+  _m.count,
+  _m.doneCsv
+);
+router.get("/count", _m.setQuery, _m.count, _m.done);
+router.get("/:_id", _m.setQuery, _m.read, _m.done);
+router.post("/create", _m.protect, _m.create, _m.done);
+router.put("/:_id", _m.protect, _m.setQuery, _m.update, _m.done);
+router.delete(
+  "/:_id",
+  _m.protect,
+  _m.setQuery,
+  _m.update,
+  _m.delete,
+  _m.create,
+  _m.done
+);
 
 module.exports = router;
